507bb2344aef75d6dfb055ade41c9d8c67be4da8
Roman Troian <89692499+RomanAlxndrvch@users.noreply.github.com>
2022-11-28 22:01:01 -0700

    6

---
+++ src/lessons/lesson6/lsn.ts
@@ -0,0 +1,37 @@
+export default () => {
+}
+/*
+const hanna = {
+    name: 'Hanna'
+}
+
+
+const baseUserProto = {
+    defaultName: 'def',
+    showName() {
+        // @ts-ignore
+        console.log(this.name)
+    }
+}
+
+const userPrototype = {
+    defaultAge: 18
+}
+
+Object.setPrototypeOf(userPrototype, baseUserProto)
+
+const alex = {
+    name: 'Alex',
+}
+
+Object.setPrototypeOf(alex, userPrototype)
+
+// @ts-ignore
+console.log(alex.defaultAge)
+// @ts-ignore
+alex.showName()
+;*/
+
+
+
+
